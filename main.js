8f0a827e07a863009249f44ce19e925fecd25399
Berna Dereağzı <bernaarmarosss@gmail.com>
2022-02-18 16:49:58 +0300

    first commit

--- main.js
+++ main.js
@@ -15,42 +15,36 @@ booBtn.onclick=function(){
     document.body.style.backgroundColor = "crimson";
     sound.play();
 }
-
 applauseBtn.onclick=function(){
     sound.src="./sounds/applause.mp3";
     image.src="./images/applause.gif";
     document.body.style.backgroundColor = "Green";
     sound.play();
 }
-
 gaspBtn.onclick=function(){
     sound.src="./sounds/gasp.mp3";
     image.src="./images/gasping.gif";
     document.body.style.backgroundColor = "SkyBlue";
     sound.play();
 }
-
 tadaBtn.onclick=function(){
     sound.src="./sounds/tada.mp3";
     image.src="./images/tada.gif";
     document.body.style.backgroundColor = "Gold";
     sound.play();
 }
-
 victoryBtn.onclick=function(){
     sound.src="./sounds/victory.mp3";
     image.src="./images/victory.gif";
     document.body.style.backgroundColor = "MidnightBlue";
     sound.play();
 }
-
 wrongBtn.onclick=function(){
     sound.src="./sounds/wrong.mp3";
     image.src="./images/wrong.gif";
     document.body.style.backgroundColor = "Maroon";
     sound.play();
 }
-
 
 sound.onended = function(){
     image.src= "./images/sound.png";
